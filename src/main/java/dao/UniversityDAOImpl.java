795db7cc2a2fed728ea2819988683468a937ea03
Dimitry Miedviediev <dimitrymiedviediev@gmail.com>
2017-10-14 10:46:27 +0300

    BD calls to lowercase

--- src/main/java/dao/UniversityDAOImpl.java
+++ src/main/java/dao/UniversityDAOImpl.java
@@ -20,14 +20,14 @@ public class UniversityDAOImpl implements UniversityDAO {
         try {
             HashMap<String, String> pair = new HashMap<>();
             conn.setCatalog(connectionDB.getSCHEMA_NAME());
-            CallableStatement statement = conn.prepareCall("{CALL P_CREATE_UNIVERSITY(?,?,?,?,?,?)}");
-            statement.setString("vTITLE_UA", titleUa);
-            statement.setString("vCODE_UA", codeUa);
-            statement.setString("vTITLE_EN", titleEn);
-            statement.setString("vCODE_EN", codeEn);
+            CallableStatement statement = conn.prepareCall("{call p_create_university(?,?,?,?,?,?)}");
+            statement.setString("p_title_en", titleUa);
+            statement.setString("p_code_ua", codeUa);
+            statement.setString("p_title_en", titleEn);
+            statement.setString("p_code_en", codeEn);
             statement.execute();
-            pair.put("responseCode", statement.getString("vRESPONSE_CODE"));
-            pair.put("responseMessage", statement.getString("vRESPONSE_MESSAGE"));
+            pair.put("responseCode", statement.getString("p_response_code"));
+            pair.put("responseMessage", statement.getString("p_response_message"));
             list.add(pair);
         } catch (SQLException e) {
             e.printStackTrace();
@@ -41,7 +41,7 @@ public class UniversityDAOImpl implements UniversityDAO {
         Connection conn = connectionDB.getConnection();
         try {
             conn.setCatalog(connectionDB.getSCHEMA_NAME());
-            String sqlSelectQuery = "SELECT * FROM v_university WHERE id = ?";
+            String sqlSelectQuery = "select * from v_university where id = ?";
             PreparedStatement preparedStatement = conn.prepareStatement(sqlSelectQuery);
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -67,7 +67,7 @@ public class UniversityDAOImpl implements UniversityDAO {
         try {
             conn.setCatalog(connectionDB.getSCHEMA_NAME());
             Statement statement = conn.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM v_university");
+            ResultSet resultSet = statement.executeQuery("select * from v_university");
             int columnCount = resultSet.getMetaData().getColumnCount();
             while (resultSet.next()) {
                 HashMap<String, String> pair = new HashMap<>();
@@ -88,16 +88,16 @@ public class UniversityDAOImpl implements UniversityDAO {
         Connection conn = connectionDB.getConnection();
         try {
             conn.setCatalog(connectionDB.getSCHEMA_NAME());
-            CallableStatement statement = conn.prepareCall("{CALL P_UPDATE_UNIVERSITY(?,?,?,?,?,?,?)}");
-            statement.setInt("vID", id);
-            statement.setString("vTITLE_UA", titleUa);
-            statement.setString("vCODE_UA", codeUa);
-            statement.setString("vTITLE_EN", titleEn);
-            statement.setString("vCODE_EN", codeEn);
+            CallableStatement statement = conn.prepareCall("{call p_update_university(?,?,?,?,?,?,?)}");
+            statement.setInt("p_id", id);
+            statement.setString("p_title_en", titleUa);
+            statement.setString("p_code_ua", codeUa);
+            statement.setString("p_title_en", titleEn);
+            statement.setString("p_code_en", codeEn);
             statement.execute();
             HashMap<String, String> pair = new HashMap<>();
-            pair.put("responseCode", statement.getString("vRESPONSE_CODE"));
-            pair.put("responseMessage", statement.getString("vRESPONSE_MESSAGE"));
+            pair.put("responseCode", statement.getString("p_response_code"));
+            pair.put("responseMessage", statement.getString("p_response_message"));
             list.add(pair);
         } catch (SQLException e) {
             e.printStackTrace();
@@ -111,13 +111,13 @@ public class UniversityDAOImpl implements UniversityDAO {
         Connection conn = connectionDB.getConnection();
         try {
             conn.setCatalog(connectionDB.getSCHEMA_NAME());
-            CallableStatement statement = conn.prepareCall("{CALL P_DELETE_UNIVERSITY(?,?,?)}");
-            statement.setInt("vID", id);
+            CallableStatement statement = conn.prepareCall("{call p_delete_university(?,?,?)}");
+            statement.setInt("p_id", id);
             statement.execute();
 
             HashMap<String, String> pair = new HashMap<>();
-            pair.put("responseCode", statement.getString("vRESPONSE_CODE"));
-            pair.put("responseMessage", statement.getString("vRESPONSE_MESSAGE"));
+            pair.put("responseCode", statement.getString("p_response_code"));
+            pair.put("responseMessage", statement.getString("p_response_message"));
             list.add(pair);
         } catch (SQLException e) {
             e.printStackTrace();
