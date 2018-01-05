7f6f53ef478e0a3d8447828ad345c494ee594dc8
Dimitry Miedviediev <dimitrymiedviediev@gmail.com>
2018-01-05 15:01:37 +0200

    Dont work again

--- src/main/java/service/Application.java
+++ src/main/java/service/Application.java
@@ -1,20 +1,26 @@
 package service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Dimitry on 22.09.17.
  */
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer{
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(Application.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
